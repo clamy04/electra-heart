a909e226f9b1735990df5f69c4954d644493a8b5
Clarouf la ouf <lamyclara86@gmail.com>
2021-04-06 19:43:08 +0200

    Fonction déplacée du HTML au JS en ENTIER

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,7 +1,7 @@
-function hide() {
+document.querySelector(".translate").addEventListener("click", function () {
   var h = document.getElementById("visible");
   var s = document.getElementById("hidden");
   h.classList.replace("visible", "hidden");
   h.id = "hidden";
   s.id = "visible";
-}
+});
